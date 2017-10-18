f541c4b1e995fb796425e7d96c7b5c8b25767f63
Dominic Ma <dominic.c.ma@gmail.com>
2017-10-18 15:48:34 -0700

    Added basic express server wireframe

---
+++ server/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+
+
+// Port
+const port = process.env.PORT || 1337;
+
+let app = express();
+
+// Serve up static files
+app.use(express.static(path.join(__dirname, '../client/public/dist')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/public/dist/index.html'));
+})
+
+app.listen(port, () => {
+  console.log('Running on ' + port);
+})
