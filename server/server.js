fbd04625156f8ab12fcc092d8bf23d68ea6ceb50
Kent Shepard <kentshepard7@gmail.com>
2017-10-18 22:41:52 -0400

    Add morgan, require passport local jwt

--- server/server.js
+++ server/server.js
@@ -1,14 +1,34 @@
 var express = require('express');
 var path = require('path');
-
+var morgan = require('morgan');
+var jwt = require('jsonwebtoken');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var JwtStrategy = require('passport-jwt');
 
 // Port
 const port = process.env.PORT || 1337;
 
 let app = express();
 
+app.use(morgan('[:date[clf]] | ":method :url" | STATUS: :status :res[content-length] ":referrer"'));
+
+// Auth
+passport.use(new LocalStrategy(
+  function(email, password, done) {
+    // User.findOne({ username: username }, function (err, user) {
+    //   if (err) { return done(err); }
+    //   if (!user) { return done(null, false); }
+    //   if (!user.verifyPassword(password)) { return done(null, false); }
+    //   return done(null, user);
+    // });
+  }
+));
+
 // Serve up static files
 app.use(express.static(path.join(__dirname, '../client/public/dist')));
+
+// app.get('/home',)
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/public/dist/index.html'));
