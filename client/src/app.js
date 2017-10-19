8def430547ce125d60f13803fdece75755393e2d
Jared Meek <jmeek13@gmail.com>
2017-10-18 21:00:07 -0700

    added top-level app component and updated some references

--- client/src/app.js
+++ client/src/app.js
@@ -1,13 +1,23 @@
 import angular from 'angular';
 import uirouter from '@uirouter/angularjs';
 
-import routing from './app-routing';
+// import other modules
+import CommonModule from './modules/common/common';
+import HomeModule from './modules/home/home';
 
-import homeModule from './modules/home/home';
+// imports for this component
+import appRouting from './app.routing';
+import template from './app.html';
+import './app.css';
 
+const AppComponent = {
+  template: template
+};
 
 angular.module('app', [
   uirouter,
-  homeModule.name
+  CommonModule,
+  HomeModule,
 ])
-  .config(routing);
+  .config(appRouting)
+  .component('app', AppComponent);
