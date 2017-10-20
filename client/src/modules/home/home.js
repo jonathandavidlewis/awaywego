1b8c4c6e74745d4e45e87aebafeb011b2c61703a
Jonathan Lewis <jonathandavidlewis@gmail.com>
2017-10-20 16:40:06 -0700

    Adds basic sample plan cards to home screen

--- client/src/modules/home/home.js
+++ client/src/modules/home/home.js
@@ -1,12 +1,26 @@
 import angular from 'angular';
+// components used by this module
+import PlanCardComponent from './plan-card/plan-card';
 
+import PlanService from './home-services/plan.service';
 // imports for this component
 import template from './home.html';
 import './home.css';
 
 class HomeController {
-  constructor() {
-    this.name = 'Welcome to the home screen.';
+  constructor(PlanService) {
+    this.name = 'Your Plans';
+    this.PlanService = PlanService;
+    this.plans = [
+      {
+        title: "planTitle",
+        description: "planDescription"
+      },
+      {
+        title: "plan 2",
+        description: "planDescription 2"
+      }
+    ]
   }
 }
 HomeController.$inject = [];
@@ -19,6 +33,8 @@ const HomeComponent = {
 };
 
 const HomeModule = angular.module('app.home', [])
-  .component('home', HomeComponent);
+  .component('home', HomeComponent)
+  .component('planCard', PlanCardComponent)
+  .service('PlanService', PlanService);
 
 export default HomeModule.name;
