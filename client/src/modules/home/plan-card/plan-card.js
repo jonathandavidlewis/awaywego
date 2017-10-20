b4b1c11b6f47dd0208530606434e3749e477aa02
Jonathan Lewis <jonathandavidlewis@gmail.com>
2017-10-20 16:40:06 -0700

    Adds basic sample plan cards to home screen

---
+++ client/src/modules/home/plan-card/plan-card.js
@@ -0,0 +1,24 @@
+import angular from 'angular';
+
+// imports for this component
+import template from './plan-card.html';
+//import './home.css';
+
+class PlanCardController {
+  constructor() {
+    this.name = 'This is a plan title';
+  }
+}
+PlanCardController.$inject = [];
+
+const PlanCardComponent = {
+  restrict: 'E',
+  bindings: {
+    plan: '<'
+  },
+  template: template,
+  controller: PlanCardController
+};
+
+
+export default PlanCardComponent;
