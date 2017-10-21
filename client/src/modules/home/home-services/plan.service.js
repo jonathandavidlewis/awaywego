1b2533fa6a45d35e6fb2b55a422047a51232c79c
Jonathan Lewis <jonathandavidlewis@gmail.com>
2017-10-21 02:08:51 -0700

    Adds Getplan service and styling for make plan

--- client/src/modules/home/home-services/plan.service.js
+++ client/src/modules/home/home-services/plan.service.js
@@ -3,5 +3,25 @@ export default class PlanService {
     this.$inject = ['$http'];
 
   }
+  getPlan(endpoint, params, callback) {
+    $http({
+      method: 'GET',
+      url: endpoint,
+      params: params
+    }).then(function successCallback(response) {
+      callback(response.data);
+    }, function errorCallback(response) {
+      console.log(response);
+    });
+  };
+
+  submitNewPlan(plan) {
+    return $http({
+      method: 'POST',
+      url: '/api/plan',
+      params: plan
+    })
+  };
+
 
 }
