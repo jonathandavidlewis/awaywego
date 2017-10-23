da9a87b052fca4f6e593973bd0d200b260d7c93a
Jared Meek <jmeek13@gmail.com>
2017-10-23 15:39:08 -0700

    adding friend router to api router

--- server/api/api-router.js
+++ server/api/api-router.js
@@ -1,6 +1,8 @@
 const apiRouter = require('express').Router();
 const planRouter = require('./plan-router');
+const friendRouter = require('./friend-router');
 
 apiRouter.use('/plan', planRouter);
+apiRouter.use('/friends', friendRouter);
 
 module.exports = apiRouter;
