eec228035b8c89fa6faead6b456217b89de72fb5
Jared Meek <jmeek13@gmail.com>
2017-10-23 15:49:21 -0700

    new friend post route

--- server/api/friend-router.js
+++ server/api/friend-router.js
@@ -11,6 +11,24 @@ friendRouter.get('/', (req, res) => {
   });
 });
 
+// post - new friend request to existing user
+friendRouter.post('/new/:friendId', (req, res) => {
+  const fromId = oid(req.user._id);
+  const toId = oid(req.params.friendId);
+  Friend.find({from: fromId, to: toId}).then(fr => {
+    if (fr) { throw new Error('fr_exists'); }
+    const newFr = { from: fromId, to: toId, status: 'Pending' };
+    return Friend.create(newFr);
+  }).then(newFr => res.status(200).json({message: 'created', frId: newFr._id}))
+    .catch(err => {
+      if (err.message === 'fr_exists') {
+        res.status(422).json('Friend request already exists');
+      } else {
+        res.status(500).json({message: 'Server error.', err});
+      }
+    });
+});
+
 
 // post - friend invite -
 
