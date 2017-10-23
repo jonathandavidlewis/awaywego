bb248179e214fdde025bbe14b43a6fffc0b2ece9
Jared Meek <jmeek13@gmail.com>
2017-10-23 16:13:22 -0700

    invite friend and accept friend request routes

--- server/api/friend-router.js
+++ server/api/friend-router.js
@@ -29,10 +29,42 @@ friendRouter.post('/new/:friendId', (req, res) => {
     });
 });
 
-
 // post - friend invite -
+// TODO: send email to the invited friend
+friendRouter.post('/invite', (req, res) => {
+  const fromId = oid(req.user._id);
+  const toEmail = req.params.toEmail;
+  Friend.find({from: fromId, toEmail: toEmail}).then(fr => {
+    if (fr) { throw new Error('fr_exists'); }
+    const newFr = { from: fromId, toEmail: toEmail, status: 'Pending' };
+    return Friend.create(newFr);
+  }).then(newFr => res.status(200).json({message: 'created', frId: newFr._id}))
+    .catch(err => {
+      if (err.message === 'fr_exists') {
+        res.status(422).json('Friend request already exists');
+      } else {
+        res.status(500).json({message: 'Server error.', err});
+      }
+    });
+});
 
 // put - accept friend request
+friendRouter.put('/accept/:frId', (req, res) => {
+  const toId = oid(req.user._id);
+  Friend.findOneAndUpdate(
+    {_id: oid(req.params.frId), to: toId},
+    {status: 'Accepted'}
+  ).then(fr => {
+    if (!fr) { throw new Error('fr_not_found'); }
+    res.status(200).json({message: 'updated', frId: fr._id});
+  }).catch(err => {
+    if (err.message === 'fr_not_found') {
+      res.status(404).send();
+    } else {
+      res.status(500).json({message: 'Server error.', err});
+    }
+  });
+});
 
 // put - reject friend request
 
