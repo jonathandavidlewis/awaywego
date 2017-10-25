57e2644d24a1c9cf008538e07b3935876e7f6f7a
Jonathan Lewis <jonathandavidlewis@gmail.com>
2017-10-25 04:08:45 -0700

    Merge with event ng-if

--- client/src/modules/plan/planner/ideas/ideas-card/ideas-card.js
+++ client/src/modules/plan/planner/ideas/ideas-card/ideas-card.js
@@ -5,16 +5,27 @@ import template from './ideas-card.html';
 import './ideas-card.css';
 
 class IdeasCardController {
-  constructor() {
+  constructor($scope) {
     this.planId = 'sample';
+    this.$scope = $scope;
+    this.handleDeleteClick = this.handleDeleteClick.bind(this);
+  }
+
+  handleDeleteClick() {
+    this.deleteEvent(this.idea._id);
   }
 }
-IdeasCardController.$inject = [];
+IdeasCardController.$inject = ['$scope'];
 
 const IdeasCardComponent = {
   restrict: 'E',
   bindings: {
+<<<<<<< aa4c33d80d730dd1bf24c5bbbd36e7843992e90a
     event: '<'
+=======
+    idea: '<',
+    deleteEvent: '<'
+>>>>>>> Adds live events to idea page
   },
   template: template,
   controller: IdeasCardController
