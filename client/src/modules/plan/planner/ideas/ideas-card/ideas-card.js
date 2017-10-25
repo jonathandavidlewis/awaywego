d022f49283088b4522f25ea14bc77d0315a6b3df
Dominic Ma <dominic.c.ma@gmail.com>
2017-10-25 13:34:44 -0700

    Cleaned up console logs and spacing

--- client/src/modules/plan/planner/ideas/ideas-card/ideas-card.js
+++ client/src/modules/plan/planner/ideas/ideas-card/ideas-card.js
@@ -12,7 +12,6 @@ class IdeasCardController {
     this.handlePromoteClick = this.handlePromoteClick.bind(this);
     this.EventService = EventService;
     this.totalVotes = 0;
-
     this.upVote = this.upVote.bind(this);
     this.downVote = this.downVote.bind(this);
   }
@@ -26,9 +25,7 @@ class IdeasCardController {
   }
 
   $onInit() {
-    console.log('this.event on init', this.event);
-    this.event = this.event;
-    this.totalVotes = this.event.upVotes.length - this.event.downVotes.length;
+    this.totalVotes = this.sumVotes();
   }
 
   sumVotes() {
@@ -36,29 +33,22 @@ class IdeasCardController {
   }
 
   upVote() {
-
-    console.log('Before upvote through event service', this.event);
     this.EventService.upvoteEvent(this.event._id).then((event) => {
-      console.log('after upvote event service', event);
       this.event.upVotes = event.data.upVotes;
       this.event.downVotes = event.data.downVotes;
-      console.log('upvote current event', this.event);
       this.totalVotes = this.sumVotes();
-    })
-      .catch(err => console.log(err));
+    });
   }
 
   downVote() {
-    console.log(this.event);
     this.EventService.downvoteEvent(this.event._id).then((event) => {
-      console.log('after upvote event service', event);
       this.event.upVotes = event.data.upVotes;
       this.event.downVotes = event.data.downVotes;
-      console.log('downvote current event', this.event);
       this.totalVotes = this.sumVotes();
     });
   }
 }
+
 IdeasCardController.$inject = ['EventService'];
 
 const IdeasCardComponent = {
