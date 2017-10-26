af37ce081afde0bce4830331c3f93054d4e904b5
Kent Shepard <kentshepard7@gmail.com>
2017-10-25 23:18:14 -0400

    Upvotes/Downvotes are dynamic

--- client/src/modules/plan/planner/idea/idea.js
+++ client/src/modules/plan/planner/idea/idea.js
@@ -11,18 +11,42 @@ class IdeaController {
     this.EventService = EventService;
     this.ideaId = $stateParams.ideaId;
     this.idea = EventService.getEvent(this.ideaId);
+    this.totalVotes = 0;
+    this.upVote = this.upVote.bind(this);
+    this.downVote = this.downVote.bind(this);
   }
 
   promoteEvent(ideaId) {
     this.EventService.promoteEvent(ideaId);
   }
 
-  upvoteEvent(ideaId) {
-    this.EventService.upvoteEvent(ideaId);
+  $onInit() {
+    this.totalVotes = this.sumVotes();
   }
 
-  downvoteEvent(ideaId) {
-    this.EventService.downvoteEvent(ideaId);
+  sumVotes() {
+    let sum = this.idea.upVotes.length - this.idea.downVotes.length;
+    if (sum > 0) {
+      return '+ ' + sum;
+    } else {
+      return sum;
+    }
+  }
+
+  upVote() {
+    this.EventService.upvoteEvent(this.idea._id).then((idea) => {
+      this.idea.upVotes = idea.data.upVotes;
+      this.idea.downVotes = idea.data.downVotes;
+      this.totalVotes = this.sumVotes();
+    });
+  }
+
+  downVote() {
+    this.EventService.downvoteEvent(this.idea._id).then((idea) => {
+      this.idea.upVotes = idea.data.upVotes;
+      this.idea.downVotes = idea.data.downVotes;
+      this.totalVotes = this.sumVotes();
+    });
   }
 }
 IdeaController.$inject = ['EventService', '$stateParams'];
