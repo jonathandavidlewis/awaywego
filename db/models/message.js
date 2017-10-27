206d950541b3d904c6f6599444bae5d4b6901021
Jared Meek <jmeek13@gmail.com>
2017-10-27 11:41:22 -0700

    message db model

---
+++ db/models/message.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const db = require('../config.js');
+const Schema = mongoose.Schema;
+
+/**===== Chat model =====**\
+ Each 'Plan' will have 1 Chat, therefore we won't need a standalone model
+ to track Chat's, we'll just link messages directly to a Plan
+ We won't keep track of members of a chat inside the chat, they are already
+ tracked inside plan and that will be the source of truth
+*/
+
+const messageSchema = new Schema({
+  planId: { type: Schema.ObjectId, ref: 'Plan', required: true },
+  userId: { type: Schema.ObjectId, ref: 'User', required: true },
+  text: String
+}, { timestamps: true });
+
+var Message = mongoose.model('Message', MessageSchema);
+module.exports = Message;
