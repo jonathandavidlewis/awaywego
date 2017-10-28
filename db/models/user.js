cc6142825905e0c1c143030efe6d9aee773ec402
Jared Meek <jmeek13@gmail.com>
2017-10-27 19:30:23 -0700

    adding prof pic to user db

--- db/models/user.js
+++ db/models/user.js
@@ -7,7 +7,8 @@ const Promise = require('bluebird');
 let userSchema = new Schema({
   name: {type: String, required: true},
   email: {type: String, required: true, unique: true},
-  password: {type: String, required: true}
+  password: {type: String, required: true},
+  profilePic: String
 });
 
 userSchema.methods.comparePassword = function(pwd) {
