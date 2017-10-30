41e764f3c6a90a822b81a39d75d9dff937e3ea2e
Jared Meek <jmeek13@gmail.com>
2017-10-30 00:35:54 -0700

    refactored chat socket implementation for robustness

--- server/sockets/plan-chat-sockets.js
+++ server/sockets/plan-chat-sockets.js
@@ -9,6 +9,15 @@ module.exports = (io) => {
   io.of('/chat').on('connection', socket => {
     if (debug) { console.log('socket connected: ', socket.id); }
 
+    // initial connection setup call and response
+    if (!socketUserMap[socket]) {
+      socket.emit('id yourself');
+    }
+
+    if (!socketPlanMap[socket]) {
+      socket.emit('pick room');
+    }
+
     socket.on('id user', user => {
       socketUserMap[socket.id] = user;
       if (debug) { console.log('Connected users: ', socketUserMap); }
@@ -16,22 +25,12 @@ module.exports = (io) => {
 
     socket.on('enter plan-chat', plan => { // entering a specific plan based on planid
       if (debug) { console.log('user entered plan: ', plan); }
+      const user = socketUserMap[socket.id];
       socket.join(plan);
       socketPlanMap[socket.id] = plan;
-
-      if (chats[plan]) {
-        chats[plan].connected.push(socketUserMap[socket.id]);
-      } else {
-        chats[plan] = {connected: [socketUserMap[socket.id]], typing: []};
-      }
+      addUserToPlan(socket, plan, user);
       if (debug) { console.log('SocketPlans: ', socketPlanMap); }
       if (debug) { console.log('PlanChats: ', chats); }
-    });
-
-    socket.on('leave plan-chat', plan => {
-      if (debug) { console.log('user left plan: ', plan); }
-      cleanupSocket(socket);
-      socket.leave(plan);
     });
 
     socket.on('new message', plan => {
@@ -41,16 +40,21 @@ module.exports = (io) => {
 
     socket.on('started typing', plan => {
       if (debug) { console.log('user: ', socketUserMap[socket.id], ' started typing in plan: ', plan); }
-      if (chats[plan] && chats[plan].typing) {
-        chats[plan].typing.push(socketUserMap[socket.id]);
-        socket.to(plan).emit('users typing', chats[plan].typing);
+      const user = socketUserMap[socket.id];
+      if (chats[plan]) {
+        const chatTyping = findUserIndex(chats[plan].typing, user);
+        if (chatTyping === -1) {
+          chats[plan].typing.push(socketUserMap[socket.id]);
+          socket.to(plan).emit('users typing', chats[plan].typing);
+        }
       }
     });
 
     socket.on('stopped typing', plan => {
       if (debug) { console.log('user stopped typing in plan: ', plan); }
+      const user = socketUserMap[socket.id];
       if (chats[plan] && chats[plan].typing) {
-        const chatTyping = chats[plan] && chats[plan].typing.findIndex(u => u && u.id === socketUserMap[socket.id].id);
+        const chatTyping = findUserIndex(chats[plan].typing, user);
         if (chatTyping > -1) { chats[plan].typing.splice(chatTyping, 1); }
         socket.to(plan).emit('users typing', chats[plan].typing);
       }
@@ -58,9 +62,25 @@ module.exports = (io) => {
 
     socket.on('disconnect', () => {
       if (debug) { console.log('user disconnected...'); }
-      cleanupSocket(socket);
+      const user = socketUserMap[socket.id];
+      cleanupSocket(socket, user);
     });
   });
+
+  const findUserIndex = (arr, user) => {
+    return arr.findIndex(u => u && u.id === user.id);
+  };
+
+  const addUserToPlan = (socket, plan, user) => {
+    if (chats[plan]) {
+      const chatConnection = findUserIndex(chats[plan].connected, user);
+      if (chatConnection === -1) { // only add if not already there
+        chats[plan].connected.push(socketUserMap[socket.id]);
+      }
+    } else {
+      chats[plan] = {connected: [socketUserMap[socket.id]], typing: []};
+    }
+  };
 
   const cleanupSocket = (socket) => {
     const user = socketUserMap[socket.id];
@@ -69,16 +89,14 @@ module.exports = (io) => {
     const plan = socketPlanMap[socket.id];
     delete socketPlanMap[socket.id];
 
-    if (chats[plan] && chats[plan].typing && chats[plan].connected) {
-      const chatConnection = chats[plan].connected.findIndex(u => u && u.id === user.id);
+    if (chats[plan]) {
+      const chatConnection = findUserIndex(chats[plan].connected, user);
       if (chatConnection > -1) { chats[plan].connected.splice(chatConnection, 1); }
 
-      const chatTyping = chats[plan].typing.findIndex(u => u && u.id === user.id);
+      const chatTyping = findUserIndex(chats[plan].typing, user);
       if (chatTyping > -1) { chats[plan].typing.splice(chatTyping, 1); }
 
-      if (chats[plan].connected.length === 0) {
-        delete chats[plan];
-      }
+      if (chats[plan].connected.length === 0) { delete chats[plan]; }
     }
     return user;
   };
