f7899f63474c5feadc11c72e0b93f22e29c9fe6f
Dominic Ma <dominic.c.ma@gmail.com>
2017-10-30 21:17:44 -0700

    Updated expense and transaction to cross reference each other

--- db/models/transaction.js
+++ db/models/transaction.js
@@ -6,8 +6,9 @@ const Schema = mongoose.Schema;
 const transactionSchema = new Schema({
   from: { type: Schema.ObjectId, ref: 'User', required: true },
   to: { type: Schema.ObjectId, ref: 'User', required: true },
-  amount: Number
+  amount: Number,
+  expenseId: { type: Schema.ObjectId, ref: 'Expense', required: true}
 }, { timestamps: true });
 
-var Friend = mongoose.model('Friend', friendSchema);
-module.exports = Friend;
+var Transaction = mongoose.model('Transaction', transactionSchema);
+module.exports = Transaction;
