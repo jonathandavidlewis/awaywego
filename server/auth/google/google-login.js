b9a9af7a26c852b43f36870c9dcba1836386a23c
Jonathan Lewis <jonathandavidlewis@gmail.com>
2017-10-31 15:41:09 -0700

    Sets keys to export

--- server/auth/google/google-login.js
+++ server/auth/google/google-login.js
@@ -13,27 +13,20 @@ const loginGoogleUser = (req, res) => {
   if (req.user.photos && req.user.photos[0]) {
     newUser.profilePic = req.user.photos[0].value;
   }
-  console.log('ARRIVED_01');
 
   User.findOne({email: email}).then((user) => {
     if (user) {
-      console.log('ARRIVED_ERR');
       const token = jwt.sign({name: user.name, userId: user._id, email: user.email}, jwtOptions.secretOrKey);
-      console.log(token, typeof token);
       res.locals.newToken = token;
       res.render('index');
     } else {
-      console.log('ARRIVED_02');
       User.create(newUser).then((user) => {
-        console.log('ARRIVED_02_01');
         const token = jwt.sign({name: user.name, userId: user._id, email: user.email}, jwtOptions.secretOrKey);
-        console.log(token, typeof token);
         res.render('index', {
           helpers: {
             newToken: () => token
           }
         });
-        console.log('ARRIVED_04');
         // once signed up - see if anyone has invited this user
         Friend.update({toEmail: user.email}, {toEmail: '', to: oid(user._id)})
           .catch(err => console.log('Error updating invite-signup: ', err));
