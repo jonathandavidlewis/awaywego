86ce7918976682e12b808d811f578d37224d5a34
Jared Meek <jmeek13@gmail.com>
2017-10-30 21:31:29 -0700

    connecting event sockets to server

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ const http = require('http');
 const port = process.env.PORT || 8080;
 const socketio = require('socket.io');
 const chatSockets = require('./sockets/plan-chat-sockets.js');
+const eventSockets = require('./sockets/event-sockets.js');
 
 const server = http.createServer(app);
 const io = socketio(server);
 app.io = io;
 chatSockets(io);
+eventSockets(io);
 
 server.listen(port, () => console.log('listening on port: ', port));
