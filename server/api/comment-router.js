0337b232705f24ef843d0e4b870a6fe99f8dd141
Jared Meek <jmeek13@gmail.com>
2017-10-30 17:07:07 -0700

    added debugs and fixed perm check error

--- server/api/comment-router.js
+++ server/api/comment-router.js
@@ -3,6 +3,7 @@ const Plan = require('../../db/models/plan.js');
 const PlanEvent = require('../../db/models/event.js');
 const Comment = require('../../db/models/comment.js');
 const oid = require('mongoose').Types.ObjectId;
+const debug = process.env.DEBUG || true;
 
 commentRouter.all('/:planEventId', (req, res, next) => {
   permissionsCheck(req.user._id, req.params.planEventId)
@@ -11,7 +12,7 @@ commentRouter.all('/:planEventId', (req, res, next) => {
 });
 
 commentRouter.get('/:planEventId', (req, res) => {
-  Comment.find({eventId: oid(req.params.eventId)})
+  Comment.find({eventId: oid(req.params.planEventId)})
     .sort('-createdAt').limit(10)
     .populate('user', '-password').exec()
     .then(comments => res.status(200).json(comments))
@@ -19,6 +20,7 @@ commentRouter.get('/:planEventId', (req, res) => {
 });
 
 commentRouter.post('/:planEventId', (req, res) => {
+  if (debug) { console.log('Comment post received for event: ', req.params.planEventId, ', body: ', req.body); }
   let newComment = {
     eventId: req.params.planEventId,
     user: req.user._id,
@@ -30,8 +32,8 @@ commentRouter.post('/:planEventId', (req, res) => {
 });
 
 commentRouter.put('/:planEventId/:commentId', (req, res) => {
-  let newText = req.body.text;
-  Comment.findByIdAndUpdate(req.params.commentId, {text: newText})
+  if (debug) { console.log('Comment update received for event: ', req.params.planEventId, ' comment: ', req.params.commentId, ', body: ', req.body); }
+  Comment.findByIdAndUpdate(req.params.commentId, {text: req.body.text})
     .then(updatedComment => {
       if (!updatedComment) { throw new Error('comment_not_found'); }
       res.status(200).json({message: 'updated', updatedComment});
@@ -39,6 +41,7 @@ commentRouter.put('/:planEventId/:commentId', (req, res) => {
 });
 
 commentRouter.delete('/:planEventId/:commentId', (req, res) => {
+  if (debug) { console.log('Comment delete received for event: ', req.params.planEventId, ' comment: ', req.params.commentId); }
   Comment.findByIdAndRemove(req.params.commentId).then(removedComment => {
     res.status(200).json({message: 'removed', removedComment});
   }).catch(err => handleError(req, res, err));
@@ -47,15 +50,13 @@ commentRouter.delete('/:planEventId/:commentId', (req, res) => {
 // check if user is part of the group the event is in
 // if no, throw an error, if yes let it through
 const permissionsCheck = (userId, eventId) => {
-  let resEvent = null;
   return PlanEvent.findById(eventId).then(event => {
     if (!event) { throw new Error('event_not_found'); }
-    resEvent = event;
-    Plan.findById(event.planId).then(plan => {
+    return Plan.findById(event.planId).then(plan => {
       if (!plan) { throw new Error('plan_not_found'); }
       if (plan.members.indexOf(userId) === -1) { throw new Error('not_member'); }
     });
-  }).then(() => resEvent);
+  });
 };
 
 const handleError = (req, res, err) => {
