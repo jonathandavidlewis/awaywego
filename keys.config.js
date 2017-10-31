d958efa79ded67cc85dcb6898f11c99794776a48
Jared Meek <jmeek13@gmail.com>
2017-10-30 22:27:55 -0700

    resetting default keys file

--- keys.config.js
+++ keys.config.js
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 module.exports = {
   IMAGE_API_KEY: 'xxxxx',
   GOOGLE_CLIENT_ID: 'xxxxx',
   GOOGLE_CLIENT_SECRET: 'xxxxx'
+=======
+export default {
+  IMAGE_API_KEY: ''
+>>>>>>> resetting default keys file
 };
