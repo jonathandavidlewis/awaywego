8fc394f8864c1aa245173efac297eb8e61e14ea5
Jonathan Lewis <jonathandavidlewis@gmail.com>
2017-10-30 23:48:42 -0700

    Reorganizes auth and correctly calls Google

---
+++ server/auth/auth-utils.js
@@ -0,0 +1,40 @@
+const oid = require('mongoose').Types.ObjectId;
+const Friend = require('../../db/models/friend.js');
+const User = require('../../db/models/user.js');
+const debug = process.env.DEBUG || false;
+const jwt = require('jsonwebtoken');
+const { jwtAuth, pwdAuth, jwtOptions } = require('./auth-config.js');
+
+const loginUser = (req, res) => {
+  if (debug) { console.log('login attempt for: ', req.body.email); }
+  const token = jwt.sign({
+    name: req.user.name,
+    userId: req.user._id,
+    email: req.user.email
+  }, jwtOptions.secretOrKey);
+  res.json({message: 'Log In was successful', token: token});
+};
+
+const createUser = (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const newUser = {name: name, email: email, password: password};
+
+  User.findOne({email: email}).then((user) => {
+    if (user) {
+      res.status(422).json({message: 'User already exists'});
+    } else {
+      User.create(newUser).then((user) => {
+        const token = jwt.sign({name: user.name, userId: user._id, email: user.email}, jwtOptions.secretOrKey);
+        res.status(201).json({message: 'Registration was successful', token: token});
+        // once signed up - see if anyone has invited this user
+        Friend.update({toEmail: user.email}, {toEmail: '', to: oid(user._id)})
+          .catch(err => console.log('Error updating invite-signup: ', err));
+      });
+    }
+  }).catch((err) => res.status(500).json({message: err}));
+};
+
+module.exports.loginUser = loginUser;
+module.exports.createUser = createUser;
--- server/auth/google/google-router.js
+++ server/auth/google/google-router.js
@@ -6,9 +6,29 @@ const Friend = require('../../../db/models/friend.js');
 const oid = require('mongoose').Types.ObjectId;
 const debug = process.env.DEBUG || false;
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET} = require('../../../keys.config.js');
+const passport = require('passport');
 
 
+passport.use(new GoogleStrategy({
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: '/auth/google/callback'
+}, function(req, accessToken, refreshToken, profile, done) {
 
+  return done(null, profile);
+}
+));
+
+
+/* todo: Create the user or log them in.
+          User.findOrCreate({googleId: profile.id}, function(err, user) {
+            if (err) {
+              return console.error(err);
+            }
+            //call done here;
+          });
+    */
 
 //redirect to google.
 //when return:
@@ -16,18 +36,21 @@ const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 // if so, add google to THAT account.
 // Else, create a NEW account id and add this google id to the account.
 
-router.route('/')
+googleRouter.route('/')
   .get(passport.authenticate('google', {
-    scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']
+    scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/contacts.readonly']
   }));
 
 
 googleRouter.get( '/callback', passport.authenticate('google', {
   //successRedirect: '/',
-  failureRedirect: '/',
+  failureRedirect: '/error/',
   session: false
 }), function(req, res) {
-  var token = AuthService.encode(req.user);
+  console.log('USER IS______________', req.user);
+  console.log(JSON.stringify(req.user.emails[0].value));
+  //req.user is the profile...
+  var token = jwtAuth.encode(req.user);
   res.redirect('/home?token=' + token);
 });
 
