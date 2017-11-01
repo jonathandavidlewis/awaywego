90311955d96c7c4b8f0d5ead245a921e31a929c1
Kent Shepard <kentshepard7@gmail.com>
2017-11-01 17:33:58 -0400

    Empty searchRouter

--- server/api/search-router.js
+++ server/api/search-router.js
@@ -1,10 +1,3 @@
 const searchRouter = require('express').Router();
 
-// Location routes
-searchRouter.get('/:latitude/:longitude', (req, res) => {
-  let src = `https://maps.googleapis.com/maps/api/staticmap?center=${req.params.latitude},${req.params.longitude}&zoom=10&size=400x400&key=AIzaSyC4JL3R71CHTNuCs3r0LzQQ9etGL-tQEi8`;
-
-  res.status(200).json(src);
-});
-
 module.exports = searchRouter;
