5518930535889097926141b183061da311dc28a5
Jonathan Lewis <jonathandavidlewis@gmail.com>
2017-11-01 16:09:39 -0700

    Adds google contacts AJAX call

--- server/auth/google/google-router.js
+++ server/auth/google/google-router.js
@@ -1,60 +1,16 @@
 const googleRouter = require('express').Router();
-const oid = require('mongoose').Types.ObjectId;
 const debug = process.env.DEBUG || false;
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET} = require('../../../keys.config.js');
 const passport = require('passport');
 const { loginGoogleUser } = require('./google-login');
-const request = require ('request');
-const User = require('../../../db/models/user.js');
-const Friend = require('../../../db/models/friend.js');
+const createGoogleUser = require('./create-google-user');
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID || GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET || GOOGLE_CLIENT_SECRET,
   callbackURL: '/auth/google/callback'
-}, function(accessToken, nothing, refreshToken, profile, done) {
-
-  const userEmail = profile.emails[0].value;
-  const googleId = profile.id;
-  const name = profile.displayName;
-  const newUser = {
-    name: name,
-    email: userEmail,
-    googleId: googleId,
-    googleAccessToken: accessToken
-  };
-  let profilePic = null;
-  if (profile.photos && profile.photos[0]) {
-    profilePic = profile.photos[0].value;
-    newUser.profilePic = profilePic;
-  }
-
-  User.findOne({ googleId: googleId }).then((user) => {
-    if (user) {
-      user.update({googleAccessToken: accessToken}).then((user) => done(null, profile));
-    } else {
-      User.findOne({ email: userEmail }).then((user) => {
-        if (user) {
-          var googleUser = {googleId: googleId, googleAccessToken: accessToken};
-          if (!user.profilePic && profilePic) {
-            googleUser.profilePic = profilePic;
-          }
-          user.update(googleUser).then((user) => done(null, profile));
-        } else {
-          User.create(newUser).then((user) => {
-
-            // once signed up - see if anyone has invited this user
-            Friend.update({toEmail: user.email}, {toEmail: '', to: oid(user._id)})
-              .catch(err => console.log('Error updating invite-signup: ', err));
-
-            done(null, profile);
-          }).catch((error) => console.log('There was an error creating/updating Google user: ', error));
-        }
-      });
-    }
-  });
-}));
+}, createGoogleUser));
 
 googleRouter.route('/')
   .get(passport.authenticate('google', {
