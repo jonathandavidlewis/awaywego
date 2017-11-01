31943f47df1c85af06d122b1fa16f2ad68fd3d56
Jared Meek <jmeek13@gmail.com>
2017-11-01 12:41:58 -0700

    giving groupcard access to group service to prevent this issues

--- client/src/modules/home/group-card/group-card.js
+++ client/src/modules/home/group-card/group-card.js
@@ -5,16 +5,17 @@ import template from './group-card.html';
 import './group-card.css';
 
 class GroupCardController {
-  constructor(UserService) {
+  constructor(UserService, GroupService) {
     this.UserService = UserService;
+    this.GroupService = GroupService;
   }
 }
+GroupCardController.$inject = ['UserService', 'GroupService'];
 
 const GroupCardComponent = {
   restrict: 'E',
   bindings: {
-    group: '<',
-    deleteGroup: '<'
+    group: '<'
   },
   template: template,
   controller: GroupCardController
