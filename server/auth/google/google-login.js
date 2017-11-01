c4978adeb76ef98176036f5407b25de3c5ddca19
Jonathan Lewis <jonathandavidlewis@gmail.com>
2017-11-01 08:29:01 -0700

    Adds Google access token to Google user login

--- server/auth/google/google-login.js
+++ server/auth/google/google-login.js
@@ -10,27 +10,15 @@ const loginGoogleUser = (req, res) => {
   const email = req.user.emails[0].value;
   const googleId = req.user.id;
   const newUser = {name: name, email: email, googleId: googleId};
-  if (req.user.photos && req.user.photos[0]) {
-    newUser.profilePic = req.user.photos[0].value;
-  }
 
-  User.findOne({email: email}).then((user) => {
+  User.findOne({googleId: googleId}).then((user) => {
     if (user) {
       const token = jwt.sign({name: user.name, userId: user._id, email: user.email}, jwtOptions.secretOrKey);
       res.locals.newToken = token;
+      res.locals.googleAccessToken = user.googleAccessToken;
       res.render('index');
     } else {
-      User.create(newUser).then((user) => {
-        const token = jwt.sign({name: user.name, userId: user._id, email: user.email}, jwtOptions.secretOrKey);
-        res.render('index', {
-          helpers: {
-            newToken: () => token
-          }
-        });
-        // once signed up - see if anyone has invited this user
-        Friend.update({toEmail: user.email}, {toEmail: '', to: oid(user._id)})
-          .catch(err => console.log('Error updating invite-signup: ', err));
-      }).catch((err) => res.status(500).json({message: err}));
+      console.log('There should have been a Google user, but none was found');
     }
   }).catch((err) => res.status(500).json({message: err}));
 };
