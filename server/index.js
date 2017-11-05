1f371c6b76e2947aba611a7089ad722a8ac7d4eb
Jonathan Lewis <jonathandavidlewis@gmail.com>
2017-11-05 10:28:18 -0800

    Adds initial https

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const app = require('./server.js');
 const http = require('http');
+const https = require('https');
 const port = process.env.PORT || 8080;
 const socketio = require('socket.io');
 const chatSockets = require('./sockets/group-chat-sockets.js');
 const eventSockets = require('./sockets/event-sockets.js');
+const fs = require('fs');
+
+const privateKey = fs.readFileSync('myserver.key', 'utf8');
+const certificate = fs.readFileSync('domain.crt', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
 
 const server = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 const io = socketio(server);
 app.io = io;
 chatSockets(io);
