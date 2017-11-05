6fa8a2ddd1f77c9cafd25d05a711224c265d89ff
Jared Meek <jmeek13@gmail.com>
2017-11-04 19:35:07 -0700

    adding dropdown delete to comments

--- client/src/modules/comments/comment-card/comment-card.js
+++ client/src/modules/comments/comment-card/comment-card.js
@@ -5,19 +5,26 @@ import template from './comment-card.html';
 import './comment-card.css';
 
 class CommentCardController {
-  constructor(UserService, MomentService, EventService) {
+  constructor(UserService, MomentService, EventService, GroupService) {
     this.user = UserService.user;
     this.moment = MomentService.moment;
     this.EventService = EventService;
+    this.groupOwner = GroupService.currentGroup.userId;
     this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    this.canEditOrDelete = this.canEditOrDelete.bind(this);
   }
 
   handleDeleteClick() {
     this.EventService.removeCommentForEvent(this.eventId, this.comment._id);
   }
 
+  canEditOrDelete() {
+    return this.EventService.events[this.eventId].userId === this.user.id ||
+           this.userId === this.groupOwner;
+  }
+
 }
-CommentCardController.$inject = ['UserService', 'MomentService', 'EventService'];
+CommentCardController.$inject = ['UserService', 'MomentService', 'EventService', 'GroupService'];
 
 const CommentCardComponent = {
   restrict: 'E',
