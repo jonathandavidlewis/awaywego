729f98e68382f61971075d5ec0ef330d636a44a8
Jared Meek <jmeek13@gmail.com>
2017-11-07 01:57:18 -0800

    refactoring people card to own more responsibility

--- client/src/modules/group/people/people-card/people-card.js
+++ client/src/modules/group/people/people-card/people-card.js
@@ -5,29 +5,45 @@ import template from './people-card.html';
 import './people-card.css';
 
 class PeopleCardController {
-  constructor() {
-    this.handleContainerClick = this.handleContainerClick.bind(this);
+  constructor(GroupService, ConfirmService) {
+    this.GroupService = GroupService;
+    this.ConfirmService = ConfirmService;
   }
 
-  handleContainerClick() {
-    if (this.type === 'select') {
-      this.toggle(this.user._id);
-    }
+  remove() {
+    this.ConfirmService.openModal(
+      'Are you sure you want to remove this user from the group?',
+      'This action cannot be undone', 'Yes'
+    ) .then(() => {
+      this.busy = true;
+      this.GroupService.removeMemberFromCurrentGroup(this.member._id)
+        .finally(() => this.busy = false);
+    }).catch(() => {});
   }
 
+  canRemoveOrSelect() {
+    return this.ownerId === this.user.id && this.member._id !== this.user.id;
+  }
 }
-PeopleCardController.$inject = [];
+PeopleCardController.$inject = ['GroupService', 'ConfirmService'];
+
+/* Bindings overview
+  type: select -> indicates card can be selected (for adding people to a group)
+        null -> card in list mode, can remove if member !== user && user === admin or higher
+  member: the member object represented by the current card
+  owner: the group owner's id
+  user: the current user's id
+  selected: in a select sitaution, identifies if 'selected' or not
+*/
 
 const PeopleCardComponent = {
   restrict: 'E',
   bindings: {
     type: '@',
+    member: '<',
+    ownerId: '<',
     user: '<',
-    owner: '<',
-    self: '<',
-    remove: '<',
-    toggle: '<',
-    checked: '<'
+    selected: '<'
   },
   template: template,
   controller: PeopleCardController
