c1b5234e0b5ccfd59d1c3b23072852bfb20e047b
Dominic Ma <dominic.c.ma@gmail.com>
2017-11-07 12:08:42 -0800

    Refactored Expenses Summary to one column

--- client/src/modules/group/expenses/expenses-main/expenses-summary/expenses-summary.js
+++ client/src/modules/group/expenses/expenses-main/expenses-summary/expenses-summary.js
@@ -5,12 +5,28 @@ import template from './expenses-summary.html';
 import './expenses-summary.css';
 
 class ExpensesSummaryController {
-  constructor(ExpensesService) {
+  constructor(ExpensesService, UserService) {
     this.ExpensesService = ExpensesService;
+    this.UserService = UserService;
+    this.owe = 'owes';
+
+    this.isCurrentUser = this.isCurrentUser.bind(this);
   }
+
+  isCurrentUser(user, from) {
+    if (user._id === this.UserService.user.id) {
+      if (from) {
+        this.owe = 'owe';
+      }
+      return 'You';
+    } else {
+      return user.name;
+    }
+  }
+
 }
 
-ExpensesSummaryController.$inject = ['ExpensesService'];
+ExpensesSummaryController.$inject = ['ExpensesService', 'UserService'];
 
 const ExpensesSummaryComponent = {
   restrict: 'E',
