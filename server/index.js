1ec7fd44c37a926821e1f855c492ac1b34bc0b0a
Jonathan Lewis <jonathandavidlewis@gmail.com>
2017-11-07 11:30:58 -0800

    Adds http redirect

--- server/index.js
+++ server/index.js
@@ -2,20 +2,27 @@ const app = require('./server.js');
 const http = require('http');
 const https = require('https');
 const port = process.env.PORT || 8080;
+const httpsPort = process.env.HTTPS_PORT || 8443;
 const socketio = require('socket.io');
 const chatSockets = require('./sockets/group-chat-sockets.js');
 const eventSockets = require('./sockets/event-sockets.js');
 const fs = require('fs');
 
-const privateKey = fs.readFileSync('myserver.key', 'utf8');
-const certificate = fs.readFileSync('domain.crt', 'utf8');
-const credentials = {key: privateKey, cert: certificate};
+const credentials = {
+  key: fs.readFileSync('privkey.pem'),
+  cert: fs.readFileSync('fullchain.pem')
+};
 
-const server = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
-const io = socketio(server);
+const io = socketio(httpsServer);
 app.io = io;
 chatSockets(io);
 eventSockets(io);
 
-server.listen(port, () => console.log('listening on port: ', port));
+const httpsRedirect = http.createServer((req, res) => {
+  res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url }).end();
+});
+
+httpsRedirect.listen(port, () => console.log('http listening on port:', port, 'to redirect to https'));
+
+httpsServer.listen(httpsPort, () => console.log('https listening on port:', httpsPort));
