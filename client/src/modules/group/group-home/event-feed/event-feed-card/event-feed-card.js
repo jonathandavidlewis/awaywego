b102296ad1e02cb730a4a8714defd600e9cd67e4
Jared Meek <jmeek13@gmail.com>
2017-11-07 19:42:49 -0800

    adding demote event functionality for event-feed-cards

--- client/src/modules/group/group-home/event-feed/event-feed-card/event-feed-card.js
+++ client/src/modules/group/group-home/event-feed/event-feed-card/event-feed-card.js
@@ -5,11 +5,37 @@ import template from './event-feed-card.html';
 import './event-feed-card.css';
 
 class EventFeedCardController {
-  constructor(MomentService) {
+  constructor(MomentService, EventService, UserService, GroupService, ConfirmService) {
     this.moment = MomentService.moment;
+    this.EventService = EventService;
+    this.UserService = UserService;
+    this.GroupService = GroupService;
+    this.ConfirmService = ConfirmService;
+
+    this.busy = false;
+
+    this.menuShouldAppear = this.menuShouldAppear.bind(this);
+  }
+
+  menuShouldAppear() { // only group owner can unschedule
+    return this.UserService.user.id === this.GroupService.currentGroup.userId;
+  }
+
+  handleUnscheduleEvent() {
+    this.ConfirmService.openModal(
+      'Are you sure you want to unschedule this event?', '',
+      'Yes', 'No'
+    ).then(() => {
+      this.busy = true;
+      this.EventService.demoteEvent(this.event._id)
+        .finally(() => this.busy = false);
+    }).catch(() => {});
   }
 }
-EventFeedCardController.$inject = ['MomentService'];
+EventFeedCardController.$inject = [
+  'MomentService', 'EventService', 'UserService',
+  'GroupService', 'ConfirmService'
+];
 
 const EventFeedCardComponent = {
   restrict: 'E',
