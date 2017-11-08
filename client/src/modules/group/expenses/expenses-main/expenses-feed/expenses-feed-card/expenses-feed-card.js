9896c81b7cdb3e307fddb08241b6d51773ea0cf1
Dominic Ma <dominic.c.ma@gmail.com>
2017-11-07 20:10:14 -0800

    Added delete menu for expenses

--- client/src/modules/group/expenses/expenses-main/expenses-feed/expenses-feed-card/expenses-feed-card.js
+++ client/src/modules/group/expenses/expenses-main/expenses-feed/expenses-feed-card/expenses-feed-card.js
@@ -5,11 +5,35 @@ import template from './expenses-feed-card.html';
 import './expenses-feed-card.css';
 
 class ExpensesFeedCardController {
-  constructor(ExpensesService) {
+  constructor(ExpensesService, UserService, GroupService, ConfirmService) {
     this.ExpensesService = ExpensesService;
+    this.userId = UserService.user.id;
+    this.groupOwner = GroupService.currentGroup.userId;
+    this.ConfirmService = ConfirmService;
+
+
+    this.handleDelete = this.handleDelete.bind(this);
+    this.menuShouldAppear = this.menuShouldAppear.bind(this);
   }
+  handleDelete() {
+    this.ConfirmService.openModal(
+      `Are you sure you want to delete expense:
+      ${this.expense.description}?`,
+      'This action cannot be undone', 'Yes'
+    ).then(() => {
+      this.busy = true;
+      this.ExpensesService.removeExpense(this.expense._id)
+        .finally(() => this.busy = false);
+    }).catch(() => {});
+  }
+
+  menuShouldAppear() {
+    return this.expense.userId === this.userId ||
+           this.groupOwner === this.userId;
+  }
+
 }
-ExpensesFeedCardController.$inject = ['ExpensesService'];
+ExpensesFeedCardController.$inject = ['ExpensesService', 'UserService', 'GroupService', 'ConfirmService'];
 
 const ExpensesFeedCardComponent = {
   restrict: 'E',
