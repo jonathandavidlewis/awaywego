f9d15f827193f61c84815353362b33dc98cf71cf
Jared Meek <jmeek13@gmail.com>
2017-11-08 20:37:43 -0800

    spreading promote idea style changes to make-event

--- client/src/modules/group/ideas/make-event/make-event.js
+++ client/src/modules/group/ideas/make-event/make-event.js
@@ -5,54 +5,34 @@ import template from './make-event.html';
 import './make-event.css';
 
 class MakeEventController {
-  constructor($state, EventService, GroupService) {
-    this.$state = $state;
-    this.groupId = GroupService.currentGroup._id;
+  constructor($state, EventService, GroupService, MomentService) {
     this.EventService = EventService;
+    this.state = $state;
+    this.moment = MomentService.moment;
+    this.groupId = GroupService.currentGroup._id;
     this.title = '';
     this.desc = '';
     this.imageUrl = '';
     this.addressName = '';
-    this.addressName = '';
     this.addressText = '';
     this.addressLink = '';
-    this.startTime = new Date();
+    this.startTime = this.moment().startOf('hour').add(1, 'hour').toDate();
     this.endTime = null;
-    this.milliseconds = '';
-    this.endTimeChanged = false;
-    this.endDateChanged = false;
-    this.onStartDateChange = this.onStartDateChange.bind(this);
-    // this.onStartTimeChange = this.onStartTimeChange.bind(this);
-    // this.onEndDateTimeChange = this.onEndDateTimeChange.bind(this);
+    this.showEndDrawer = false;
+    this.openEndDrawer = this.openEndDrawer.bind(this);
     this.formWarning = '';
   }
 
-  // $onInit() {
-  //   // this.onStartTimeChange();
-  //   this.event = {};
-  //   this.event.groupId = this.groupId;
-  //   this.event.userId = this.UserService.user.id;
-  //   this.event.imageUrl = '';
-  //   this.event = this.EventService.getEvent(this.stateParams.eventId);
-  // }
-
-  onStartDateChange() {
-    if (!this.endDateChanged) {
-      this.milliseconds = this.startTime.getTime() + (60 * 60 * 1000);
-      this.endTime = new Date(this.milliseconds);
+  openEndDrawer() {
+    this.showEndDrawer = true;
+    if (!this.endTime) {
+      this.endTime = this.moment(this.startTime).add(1, 'hour').toDate();
     }
   }
 
-  // onStartTimeChange() {
-  //   if (!this.endTimeChanged) {
-  //     this.milliseconds = this.startTime.getTime() + (60 * 60 * 1000);
-  //     this.endTime = new Date(this.milliseconds);
-  //   }
-  // }
-
-  onEndDateTimeChange() {
-    this.endDateChanged = true;
-    this.endTimeChanged = true;
+  clearEndTime() {
+    this.showEndDrawer = false;
+    this.endTime = null;
   }
 
   submit() {
@@ -70,10 +50,7 @@ class MakeEventController {
       };
       this.EventService.submitScheduledEvent(newEvent).then(resp => {
         this.$state.go('app.group.home');
-      }).catch(err => {
-        console.log('Server error: ', err);
-        this.formWarning = 'Error: please try again or contact a server admin';
-      });
+      }).catch(err => {});
     }
   }
 
@@ -88,7 +65,7 @@ class MakeEventController {
     return true;
   }
 }
-MakeEventController.$inject = ['$state', 'EventService', 'GroupService'];
+MakeEventController.$inject = ['$state', 'EventService', 'GroupService', 'MomentService'];
 
 const MakeEventComponent = {
   restrict: 'E',
