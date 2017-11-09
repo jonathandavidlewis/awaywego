b34ca8aa7c7743cc03bd80bd89378a28a531968f
Jared Meek <jmeek13@gmail.com>
2017-11-08 20:18:58 -0800

    further tweaks to schedule event js and styling

--- client/src/modules/group/ideas/promote-idea/promote-idea.js
+++ client/src/modules/group/ideas/promote-idea/promote-idea.js
@@ -8,54 +8,30 @@ import template from './promote-idea.html';
 import './promote-idea.css';
 
 class PromoteIdeaController {
-  constructor(EventService, UserService, $stateParams, $state) {
+  constructor(EventService, $state, MomentService) {
     this.EventService = EventService;
-    this.UserService = UserService;
-    this.stateParams = $stateParams;
     this.state = $state;
-    this.formWarning = '';
+    this.moment = MomentService.moment;
     this.addressName = '';
     this.addressText = '';
     this.addressLink = '';
-    this.startTime = new Date();
+    this.startTime = this.moment().startOf('hour').add(1, 'hour').toDate();
     this.endTime = null;
-    this.milliseconds = '';
-    this.endTimeChanged = false;
-    this.endDateChanged = false;
-    this.onStartDateChange = this.onStartDateChange.bind(this);
-    this.onStartTimeChange = this.onStartTimeChange.bind(this);
-    this.onEndDateTimeChange = this.onEndDateTimeChange.bind(this);
+    this.showEndDrawer = false;
+    this.openEndDrawer = this.openEndDrawer.bind(this);
+    this.formWarning = '';
   }
 
-  // $onInit() {
-  //   this.onStartTimeChange();
-  //   if (this.stateParams.eventId === 'new') {
-  //     this.event = {};
-  //     this.event.groupId = this.stateParams.groupId;
-  //     this.event.userId = this.UserService.user.id;
-  //     this.event.imageUrl = '';
-  //   } else {
-  //     this.event = this.EventService.getEvent(this.stateParams.eventId);
-  //   }
-  // }
-
-  onStartDateChange() {
-    if (!this.endDateChanged) {
-      this.milliseconds = this.startTime.getTime() + (60 * 60 * 1000);
-      this.endTime = new Date(this.milliseconds);
+  openEndDrawer() {
+    this.showEndDrawer = true;
+    if (!this.endTime) {
+      this.endTime = this.moment(this.startTime).add(1, 'hour').toDate();
     }
   }
 
-  onStartTimeChange() {
-    if (!this.endTimeChanged) {
-      this.milliseconds = this.startTime.getTime() + (60 * 60 * 1000);
-      this.endTime = new Date(this.milliseconds);
-    }
-  }
-
-  onEndDateTimeChange() {
-    this.endDateChanged = true;
-    this.endTimeChanged = true;
+  clearEndTime() {
+    this.showEndDrawer = false;
+    this.endTime = null;
   }
 
   submit() {
@@ -69,21 +45,9 @@ class PromoteIdeaController {
       promotedIdea.addressName = this.addressName;
       promotedIdea.addressText = this.addressText;
       promotedIdea.addressLink = this.addressLink;
-      if (this.stateParams.eventId === 'new') {
-        this.EventService.submitScheduledEvent(promotedIdea).then(resp => {
-          this.state.go('app.group.home');
-        }).catch(err => {
-          console.log('Server error: ', err);
-          this.formWarning = 'Error: please try again or contact a server admin';
-        });
-      } else {
-        this.EventService.promoteEvent(promotedIdea).then(resp => {
-          this.state.go('app.group.home');
-        }).catch(err => {
-          console.log('Server error: ', err);
-          this.formWarning = 'Error: please try again or contact a server admin';
-        });
-      }
+      this.EventService.promoteEvent(promotedIdea).then(resp => {
+        this.state.go('app.group.home');
+      }).catch(() => {});
     }
   }
 
@@ -96,7 +60,7 @@ class PromoteIdeaController {
   }
 }
 
-PromoteIdeaController.$inject = ['EventService', 'UserService', '$stateParams', '$state'];
+PromoteIdeaController.$inject = ['EventService', '$state', 'MomentService'];
 
 const PromoteIdeaComponent = {
   restrict: 'E',
