471730d3d2407c879f0b316bca71c41472411bf1
Jared Meek <jmeek13@gmail.com>
2017-11-13 01:30:00 -0800

    removing https server

--- server/index.js
+++ server/index.js
@@ -1,29 +1,15 @@
 const app = require('./server.js');
 const http = require('http');
-const https = require('https');
 const port = process.env.PORT || 8080;
-const httpsPort = process.env.HTTPS_PORT || 8443;
 const socketio = require('socket.io');
 const chatSockets = require('./sockets/group-chat-sockets.js');
 const eventSockets = require('./sockets/event-sockets.js');
 const fs = require('fs');
 
-const credentials = {
-  key: fs.readFileSync('privkey.pem'),
-  cert: fs.readFileSync('fullchain.pem')
-};
-
-const httpsServer = https.createServer(credentials, app);
-const io = socketio(httpsServer);
+const server = http.createServer(app);
+const io = socketio(server);
 app.io = io;
 chatSockets(io);
 eventSockets(io);
 
-const httpsRedirect = http.createServer((req, res) => {
-  res.writeHead(301, { 'Location': 'https://' + req.headers['host'] + req.url });
-  res.end();
-});
-
-httpsRedirect.listen(port, () => console.log('http listening on port:', port, 'to redirect to https'));
-
-httpsServer.listen(httpsPort, () => console.log('https listening on port:', httpsPort));
+server.listen(port, 'localhost', () => console.log('http server listening on port: ', port));
